b137e4b31b712d563c4646c499d20caf915e315a
Devian <info@devian.tk>
2015-05-11 19:02:11 +0200

    Initial Commit

---
+++ BarChart/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BarChart
+//
+//  Created by Damiaan Dufaux on 11/05/15.
+//  Copyright (c) 2015 Damiaan Dufaux. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ BarChart/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BarChart
+//
+//  Created by Damiaan Dufaux on 11/05/15.
+//  Copyright (c) 2015 Damiaan Dufaux. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
